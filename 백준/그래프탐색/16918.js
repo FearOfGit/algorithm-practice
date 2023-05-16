0cd82caa27ac15bbb8026605b65a5ff88ea9e0d1
Gyeongm1n <affirming_km@naver.com>
2023-05-16 13:03:11 +0900

    230516

--- 백준/그래프탐색/16918.js
+++ 백준/그래프탐색/16918.js
@@ -1,47 +1,44 @@
-// https://www.acmicpc.net/problem/16918
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = require('fs')
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs")
   .readFileSync(filePath)
   .toString()
   .trim()
-  .split('\n');
+  .split("\n");
 
-const [R, C, N] = input[0].split(' ').map(Number);
-const map = Array.from({ length: R }, () => new Array(C).fill(0));
-// 폭탄이 터지는 시간을 저장할 배열
-const bombtime = Array.from({ length: R }, () => new Array(C).fill(0));
-for (let i = 1; i <= R; i++) {
-  for (let j = 0; j < C; j++) {
-    map[i - 1][j] = input[i][j];
-    if (map[i - 1][j] === 'O') {
-      bombtime[i - 1][j] = 3;
-    }
-  }
-}
-
-let time = 0;
+const [R, C, N] = input[0].split(" ").map((el) => +el);
+const map = Array.from({ length: R }, () => Array(C).fill(null));
+const bombtime = Array.from({ length: R }, () => Array(C).fill(0));
 const dir = [
   [-1, 0],
   [1, 0],
   [0, -1],
   [0, 1],
 ];
+let time = 1;
 
-while (time++ < N) {
+for (let i = 1; i <= R; i++) {
+  const temp = input[i].split("");
+  for (let j = 0; j < C; j++) {
+    map[i - 1][j] = temp[j];
+    if (temp[j] === "O") bombtime[i - 1][j] = 3;
+  }
+}
+
+while (time <= N) {
   if (time % 2 === 0) {
     for (let i = 0; i < R; i++) {
       for (let j = 0; j < C; j++) {
-        if (map[i][j] === '.') {
-          map[i][j] = 'O';
+        if (map[i][j] === ".") {
+          map[i][j] = "O";
           bombtime[i][j] = time + 3;
         }
       }
     }
-  } else if (time % 2 === 1) {
+  } else {
     for (let i = 0; i < R; i++) {
       for (let j = 0; j < C; j++) {
         if (bombtime[i][j] === time) {
-          map[i][j] = '.';
+          map[i][j] = ".";
 
           for (let k = 0; k < 4; k++) {
             const nx = i + dir[k][0];
@@ -49,8 +46,8 @@ while (time++ < N) {
 
             if (nx < 0 || ny < 0 || nx >= R || ny >= C) continue;
 
-            if (map[nx][ny] === 'O' && bombtime[nx][ny] !== time) {
-              map[nx][ny] = '.';
+            if (map[nx][ny] === "O" && bombtime[nx][ny] !== time) {
+              map[nx][ny] = ".";
               bombtime[nx][ny] = 0;
             }
           }
@@ -58,11 +55,24 @@ while (time++ < N) {
       }
     }
   }
+
+  time += 1;
 }
 
-let answer = '';
+let answer = "";
 for (let i = 0; i < R; i++) {
-  answer += map[i].join('');
-  answer += '\n';
+  answer += map[i].join("");
+  answer += "\n";
 }
+
 console.log(answer);
+
+/*
+  폭탄을 설치해놓은 초기 상태가 주어졌을 때, N초가 흐른 후의 격자판 상태를 구한다.
+
+  - bombtime 배열에 폭탄이 터져야 하는 시간을 저장한다.
+
+  - while문을 통해 N까지 time을 증가시키며 3과 4를 반복한다.
+
+  - 인접한 곳의 폭탄을 파괴시킬 때 현재 time에 폭발되어지는 폭탄은 파괴하지 않는다.
+*/
